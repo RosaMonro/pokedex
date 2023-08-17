af789b0fee6d19d480c5dee4971828b7fa0ea56e
RosaMonro <rosamaria.monro@gmail.com>
2023-08-17 20:55:03 +0200

    topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./components/Topbar/Topbar";
 import Home from "./views/Home";
-import "./globalStyles/global.scss";
+import "./scss/global.scss";
 
 function App() {
   return (
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -3,7 +3,24 @@ import "./Topbar.scss";
 export default function Topbar() {
   return (
     <>
-      <h1 className="color-red">soy la topbar al fin</h1>
+      <div className="topbar">
+        <div className="camera">
+          <div className="camera-glass">
+            <div className="camera-glass-reflex"></div>
+          </div>
+        </div>
+        <div className="lights">
+          <div className="light light--fuchsia">
+            <div className="light light--fuchsia-reflex"></div>
+          </div>
+          <div className="light light--yellow">
+            <div className="light light--yellow-reflex"></div>
+          </div>
+          <div className="light light--green">
+            <div className="light light--green-reflex"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
