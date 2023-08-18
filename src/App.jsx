b8b42aed82b3e6ce8509bc4c8dcfb4944306aa0d
RosaMonro <rosamaria.monro@gmail.com>
2023-08-18 20:35:47 +0200

    commands

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,20 @@ function App() {
     <>
       <div className="pokedex">
         <Topbar />
-        <div className="pokedex-main">
-          <div className="pokedex-screen">
+        <div className="pokedex--main">
+          <div className="pokedex--screen">
             <Routes>
               <Route path="/" element={<Home />} />
             </Routes>
           </div>
-          <div className="pokedex-commands">
-            <div className="cross-btn"></div>
-            <BtnCross />
-            <div className="btn-options"></div>
-            <Button text="HOME" />
-            <Button text="hahahahahahhahah" />
+          <div className="pokedex--commands">
+            <div className="pokedex--commands--btncross">
+              <BtnCross />
+            </div>
+            <div className="pokedex--commands--btns">
+              <Button text="HOME" />
+              <Button text="FAVS" />
+            </div>
           </div>
         </div>
       </div>
