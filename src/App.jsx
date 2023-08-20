f6d4d4f5e505147d904503ad054b832b3671a3ec
RosaMonro <rosamaria.monro@gmail.com>
2023-08-20 18:03:24 +0200

    shadow

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,30 @@ import "./scss/global.scss";
 function App() {
   return (
     <>
-      <div className="pokedex">
-        <Topbar />
-        <div className="pokedex--main">
-          <div className="pokedex--screen">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
+      <main className="main">
+        <div className="pokedex">
+          <Topbar />
+          <div className="pokedex--main">
+            <div className="pokedex--screen">
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </div>
+            <div className="pokedex--commands">
+              <div className="pokedex--commands--btncross">
+                <BtnCross />
+              </div>
+              <div className="pokedex--commands--btns">
+                <Button text="HOME" />
+                <Button text="FAVS" />
+              </div>
+            </div>
           </div>
-          <div className="pokedex--commands">
-            <div className="pokedex--commands--btncross">
-              <BtnCross />
-            </div>
-            <div className="pokedex--commands--btns">
-              <Button text="HOME" />
-              <Button text="FAVS" />
-            </div>
+          <div className="pokedex--footer">
+            <p>Pokedex 2023</p>
           </div>
         </div>
-        <div className="pokedex--footer">
-          <p>Pokedex 2023</p>
-        </div>
-      </div>
+      </main>
     </>
   );
 }
